c08c81a04e102de95f0a91238671a2f0e7393273
Maggie McCain <maggie.mccain13@gmail.com>
2016-05-27 16:23:45 -0400

    added js logic to create board

--- memory_game/main.js
+++ memory_game/main.js
@@ -2,11 +2,19 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
   alert('You found a match!');
 } else  if (cardThree === cardFour) {
   alert('You found a match!');
 } else {
   alert('Sorry, try again');
+}*/
+var board = document.getElementsByClassName('board')[0]; 
+for (x = 0; x < 4; x += 1); {
+	var cardElement = document.createElement('div');
+cardElement.className = 'card';
+board.appendChild(cardElement);
 }
-
+var createBoard = function(x) {
+  return board;
+}
